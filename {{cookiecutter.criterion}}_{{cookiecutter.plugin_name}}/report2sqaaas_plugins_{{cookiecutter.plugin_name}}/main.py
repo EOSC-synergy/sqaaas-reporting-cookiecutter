43ad3635b8709a41a12efc8c3e5f637f9ab70609
Pablo Orviz <orviz@ifca.unican.es>
2022-01-05 11:27:26 +0100

    Fix: has_threshold conditional

--- {{cookiecutter.criterion}}_{{cookiecutter.plugin_name}}/report2sqaaas_plugins_{{cookiecutter.plugin_name}}/main.py
+++ {{cookiecutter.criterion}}_{{cookiecutter.plugin_name}}/report2sqaaas_plugins_{{cookiecutter.plugin_name}}/main.py
@@ -9,7 +9,7 @@ logger = logging.getLogger('sqaaas.reporting.plugins.{{cookiecutter.plugin_name 
 class {{cookiecutter.plugin_class_name}}(sqaaas_utils.BaseValidator):
     valid = False
 
-    {%- if cookiecutter.has_threshold == false %}
+    {%- if cookiecutter.has_threshold == True %}
     @staticmethod
     def populate_parser(parser):
         parser.add_argument(
